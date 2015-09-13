7724051b3440f177ea172648137c4159f6b1bec5
Francis, Evan <Evan_Francis@intuit.com>
2015-09-13 16:41:55 -0700

    added commentsCount attribute to posts collection documents (denormalized). removed post_item helper to count comments since it's unnecessary now

--- client/templates/posts/post_item/post_item.js
+++ client/templates/posts/post_item/post_item.js
@@ -6,8 +6,5 @@ Template.postItem.helpers({
   },
   ownPost: function(){
     return this.userId === Meteor.userId();
-  },
-  commentsCount: function(){
-    return Comments.find({postId: this._id}).count();
   }
 });
--- server/fixtures.js
+++ server/fixtures.js
@@ -4,30 +4,25 @@ if (Posts.find().count() === 0) {
 
   // create two users
   var tomId = Meteor.users.insert({
-    profile: {
-      name: 'Tom Coleman'
-     }
+    profile: { name: 'Tom Coleman' }
   });
   var tom = Meteor.users.findOne(tomId);
   var sachaId = Meteor.users.insert({
-    profile: {
-      name: 'Sacha Greif'
-    }
+    profile: { name: 'Sacha Greif' }
   });
   var sacha = Meteor.users.findOne(sachaId);
 
-  //create a post
-  var telescopePostId = Posts.insert({
+  var telescopeId = Posts.insert({
     title: 'Introducing Telescope',
     userId: sacha._id,
     author: sacha.profile.name,
     url: 'http://sachagreif.com/introducing-telescope/',
-    submitted: new Date(now - 7 * 3600 * 1000)
+    submitted: new Date(now - 7 * 3600 * 1000),
+    commentsCount: 2
   });
 
-  //add comments from two users on the post
   Comments.insert({
-    postId: telescopePostId,
+    postId: telescopeId,
     userId: tom._id,
     author: tom.profile.name,
     submitted: new Date(now - 5 * 3600 * 1000),
@@ -35,28 +30,28 @@ if (Posts.find().count() === 0) {
   });
 
   Comments.insert({
-    postId: telescopePostId,
+    postId: telescopeId,
     userId: sacha._id,
     author: sacha.profile.name,
     submitted: new Date(now - 3 * 3600 * 1000),
     body: 'You sure can Tom!'
   });
 
-  //add another post
   Posts.insert({
     title: 'Meteor',
     userId: tom._id,
     author: tom.profile.name,
     url: 'http://meteor.com',
-    submitted: new Date(now - 10 * 3600 * 1000)
+    submitted: new Date(now - 10 * 3600 * 1000),
+    commentsCount: 0
   });
 
-  //add another post
   Posts.insert({
     title: 'The Meteor Book',
     userId: tom._id,
     author: tom.profile.name,
     url: 'http://themeteorbook.com',
-    submitted: new Date(now - 12 * 3600 * 1000)
+    submitted: new Date(now - 12 * 3600 * 1000),
+    commentsCount: 0
   });
 }

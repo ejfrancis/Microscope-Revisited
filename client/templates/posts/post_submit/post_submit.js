9a6b79657e813b03fd087df492596f6f3a7cf1ab
Francis, Evan <Evan_Francis@intuit.com>
2015-09-13 12:11:53 -0700

    added alert message in client if user submits repost

--- client/templates/posts/post_submit/post_submit.js
+++ client/templates/posts/post_submit/post_submit.js
@@ -11,6 +11,10 @@ Template.postSubmit.events({
       if(err){
         return alert(error.reason);
       }
+      // show this result but route anyway
+      if (result.postExists)
+        alert('This link has already been posted');
+
       Router.go('postPage', { _id: result._id});
     });
   }

d3b06cbec961ae25270eb195f987e0537829441e
Francis, Evan <Evan_Francis@intuit.com>
2015-09-13 14:14:11 -0700

    validate post contents on submission on both client and server

--- client/templates/posts/post_submit/post_submit.js
+++ client/templates/posts/post_submit/post_submit.js
@@ -7,6 +7,12 @@ Template.postSubmit.events({
       title: $(e.target).find('[name=title]').val()
     };
 
+    //validate the post, set session variables if any found, and return out
+    var errors = Validation.Posts.validatePost(post);
+    if(errors.title || errors.url){
+      return Session.set('postSubmitErrors', errors);
+    }
+
     Meteor.call('postInsert', post, function(err, result) {
       if(err){
         return ClientErrors.throwError(error.reason);
@@ -18,4 +24,20 @@ Template.postSubmit.events({
       Router.go('postPage', { _id: result._id});
     });
   }
+});
+
+Template.postSubmit.helpers({
+  errorMessage: function(field) {
+    return Session.get('postSubmitErrors')[field];
+  },
+  errorClass: function (field) {
+    //if there is an error at this field, return the error class, otherwise return no class
+    return !!Session.get('postSubmitErrors')[field] ?
+      'has-error' :
+      '';
+  }
+});
+
+Template.postSubmit.onCreated(function() {
+  Session.set('postSubmitErrors', {});
 });

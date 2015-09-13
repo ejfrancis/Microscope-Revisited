50c700a5f4fee1c6cd3679b2eb7b2986026162d4
Francis, Evan <Evan_Francis@intuit.com>
2015-09-13 12:56:43 -0700

    added edit post form

--- client/templates/posts/post_item/post_item.js
+++ client/templates/posts/post_item/post_item.js
@@ -3,5 +3,8 @@ Template.postItem.helpers({
     var a = document.createElement('a');
     a.href = this.url;
     return a.hostname;   
+  },
+  ownPost: function(){
+    return this.userId === Meteor.userId();
   }
 });

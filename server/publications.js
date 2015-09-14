a3bf075247282ba8bcbab3a6455fb5f805af6121
Francis, Evan <Evan_Francis@intuit.com>
2015-09-13 19:24:26 -0700

    added basic notifications collection and added to collection when comment is created

--- server/publications.js
+++ server/publications.js
@@ -7,3 +7,7 @@ Meteor.publish('comments', function(postId){
   check(postId, String);
   return Comments.find({postId: postId});
 });
+
+Meteor.publish('notifications', function() {
+  return Notifications.find();
+});

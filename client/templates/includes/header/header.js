0f737271f5e1eca67416d510b489de908355d818
Francis, Evan <Evan_Francis@intuit.com>
2015-09-14 16:34:13 -0700

    added header template helper to add an active class to the nav item for each route

---
+++ client/templates/includes/header/header.js
@@ -0,0 +1,14 @@
+Template.header.helpers({
+  activeRouteClass: function( /* route names */){
+    //convert
+    var args = Array.prototype.slice.call(arguments,0);
+    args.pop(); //remove the last arg added by spacebars
+
+    //if the current route's name matches any of the arguments passed in, it's active
+    var active = _.any(args, function(name){
+      return Router.current() && Router.current().route.getName() === name
+    });
+
+    return active && 'active';
+  }
+});
